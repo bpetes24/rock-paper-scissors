2eb55dfe849bb4f87a7b8a010bb2e3d154533526
Braden Peterson <braden.peterson@live.com>
2024-12-19 18:52:31 -0500

    Add getComputerChoice function declaration and console.log() for testing purposes

--- rps-game.js
+++ rps-game.js
@@ -1,3 +1,12 @@
-//JS file for running Rock, Paper, Scissors!
+//JS file for running Rock, Paper, Scissors
+/*
+* Write function: getComputerChoice
+* Randomly returns “rock”, “paper”, or “scissors” string values
+* Hint: use Math.random method (returns random number x>=0 && x<1)
+* Test function using console.log
+*/
+function getComputerChoice() {
+    //Insert code here
+} //getComputerChoice
 
-console.log("Hello World!")
+console.log(getComputerChoice());
